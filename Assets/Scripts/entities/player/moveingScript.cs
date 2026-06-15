ec213b5955aa9d3d9c99a8ed60f17bb0e37f5871
Tysi3k22 <kacpert.kasiak@gmail.com>
2026-06-15 15:03:59 +0200

    added player movement and test platform

---
+++ Assets/Scripts/entities/player/moveingScript.cs
@@ -0,0 +1,53 @@
+using UnityEngine;
+
+public class moveingScript : MonoBehaviour
+{
+    public Rigidbody2D rb;
+    public float jumpForce = 15f;
+    private float horizontal;
+    private float speed = 5f;
+    private bool isfacingRight = true;
+
+    public Transform groundCheck;
+    public LayerMask groundLayer;
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontal = Input.GetAxisRaw("Horizontal");
+
+        if (isGrounded() && Input.GetButtonDown("Jump"))
+        {
+            rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpForce);
+        }
+
+        if (Input.GetButtonUp("Jump") && rb.linearVelocity.y > 0f)
+        {
+            rb.linearVelocity = new Vector2(rb.linearVelocity.x, rb.linearVelocity.y * 0.5f);
+        }
+
+        flip();
+    }
+
+    private void FixedUpdate()
+    {
+        rb.linearVelocity = new Vector2(horizontal * speed, rb.linearVelocity.y);
+    }
+
+    private bool isGrounded()
+    {
+        return Physics2D.OverlapCircle(groundCheck.position, 0.2f, groundLayer);
+    }
+
+    private void flip()
+    {
+        if (isfacingRight && horizontal < 0f || horizontal > 0f && !isfacingRight)
+        {
+            isfacingRight = !isfacingRight;
+            Vector3 localScale = transform.localScale;
+
+            localScale.x *= -1f;
+            transform.localScale = localScale;
+        }
+    }
+}
